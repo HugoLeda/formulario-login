f74632a58e91898c7f304424e8eba7627ab76c65
João Hugo Leda <joaohugoleda2@gmail.com>
2021-01-22 20:52:20 -0300

    bug undefined

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const { static } = require('express')
 const express = require('express')
 
+const crud = require('crud')
+const cruds = new crud()
+
 const app = express()
 
 app.use('/css', express.static('css'))
 
 app.get('/', (req, res) => {
+    console.log(cruds.checkEmail('a'))
     res.sendFile(__dirname + '/view/index.html')
 })
 
---
+++ script.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `login`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(50) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL,
+  `email` VARCHAR(50) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL,
+  `senha` VARCHAR(15) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL,
+  `d_nasc` DATE NOT NULL,
+  `sexo` CHAR(1) NOT NULL,
+  `celular` VARCHAR(15) NOT NULL,
+  PRIMARY KEY (`id`)  
+)
+
+SELECT * FROM login.users;
+/*DROP TABLE LOGIN.USERS;*/
+INSERT INTO `login`.`users`
+(nome, email, d_nasc, sexo, celular)
+VALUES
+('teste', 'teste@teste', '20030816', 'M', '(14) 99794-5574');
