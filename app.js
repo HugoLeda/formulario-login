7b2c761753a15c1942e8ba21b3ecf415e8d64ec7
João Hugo Leda <joaohugoleda2@gmail.com>
2021-02-01 13:19:32 -0300

    add request

--- app.js
+++ app.js
@@ -9,9 +9,18 @@ const app = express()
 app.use('/css', express.static('css'))
 
 app.get('/', (req, res) => {
+    /*
     let results = cruds.checkEmail('valorpassado')
     console.log(results)
     res.sendFile(__dirname + '/view/index.html')
+    */
+    var request = require('request');
+    request('http://localhost:8082/usuario/login/usuario?email=teste&senha=senha', function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        var parsedWeather = JSON.parse(body);
+        console.log('A temperatura atual em São Paulo é ' + parsedWeather['main']['temp']); // Print the Temperature in the city of São Paulo
+    });
 })
 
 app.listen(5050, () => {
